32aa382272ee1a2aa7da7b237df02926a85d0a55
Nuru <ryuzakilawliet083@gmail.com>
2021-05-22 17:44:57 +0300

    modula little

--- videodetection.py
+++ videodetection.py
@@ -21,10 +21,11 @@ https://www.scantips.com/lights/subjectdistance.html
 
 INPUT_FILE='retailstore.mp4'
 OUTPUT_FILE='output.mp4'
+OUTPUT_CODEC='mp4v'
 LABELS_FILE='coco.names'
 CONFIG_FILE='yolov3.cfg'
 
-#Pretrained weights
+#Pretrained weights of yolo.
 
 WEIGHTS_FILE='yolov3.weights' 
 #WEIGHTS_FILE='yolov3-tiny.weights'
@@ -44,53 +45,31 @@ FOCAL_LENGTH = 3.5
 OBJECT_HEIGHT = 170
 
 
-fps = FPS().start()
+def yoloenv_setup():
+	LABELS = open(LABELS_FILE).read().strip().split("\n")
 
-#Output video properties
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-writer = cv2.VideoWriter(OUTPUT_FILE, fourcc, 30,
-	(800, 600), True)
+	#Deterministic random variable generation
+	np.random.seed(4)
 
-LABELS = open(LABELS_FILE).read().strip().split("\n")
+	COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+		dtype="uint8")
 
-#Deterministic random variable generation
-np.random.seed(4)
+	net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHTS_FILE)
 
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
+	# determine only the *output* layer names that we need from YOLO
+	ln = net.getLayerNames()
+	ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+	cnt =0;
+	
+	return LABELS,COLORS,net,ln
 
-net = cv2.dnn.readNetFromDarknet(CONFIG_FILE, WEIGHTS_FILE)
+def detect_from_image(image):
 
-vs = cv2.VideoCapture(INPUT_FILE)
-
-# Get the frames per second
-fps2 = vs.get(cv2.CAP_PROP_FPS) 
-
-# Get the total numer of frames in the video.
-frame_count = vs.get(cv2.CAP_PROP_FRAME_COUNT)
-print("Total frame count", frame_count)
-
-frame_number = 0
-frame_speedup = 1
-# determine only the *output* layer names that we need from YOLO
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-cnt =0;
-
-#Iterate all frames in video and search for the matching objects
-while frame_number <= frame_count:
-	frame_number += frame_speedup 
-	vs.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-	print ("Frame number", frame_number)
-	grabbed, image = vs.read()
-	if not grabbed:
-		print("")
-		break
+	LABELS,COLORS,net,ln = yoloenv_setup()
 	blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
 		swapRB=True, crop=False)
 	net.setInput(blob)
-	if W is None or H is None:
-		(H, W) = image.shape[:2]
+	H, W = image.shape[:2]
 	layerOutputs = net.forward(ln)
 
 	# initialize our lists of detected bounding boxes, confidences, and
@@ -149,28 +128,51 @@ while frame_number <= frame_count:
 			text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
 			cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
 				0.5, color, 2)
-	if len(idxs) < 4:
-  		frame_speedup = 5 - len(idxs)
-	else:
-  		frame_speedup = 1
 	# show the output image
 	if DEBUG_MODE:
 		cv2.imshow("output", cv2.resize(image,(800, 600)))
-	writer.write(cv2.resize(image,(800, 600)))
-	fps.update()
-	key = cv2.waitKey(1) & 0xFF
-	if key == ord("q"):
-		break
+	return image,len(idxs)
 
-fps.stop()
+def videostream():
+	fps = FPS().start()
+	vs = cv2.VideoCapture(INPUT_FILE)
 
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+	# Get the frames per second
+	fps2 = vs.get(cv2.CAP_PROP_FPS) 
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
+	# Get the total numer of frames in the video.
+	frame_count = vs.get(cv2.CAP_PROP_FRAME_COUNT)
+	print("Total frame count", frame_count)
 
-# release the file pointers
-print("[INFO] cleaning up...")
-writer.release()
-vs.release()
+	frame_number = 0
+	frame_speedup = 1
+	
+	#Output video properties
+	fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+	writer = cv2.VideoWriter(OUTPUT_FILE, fourcc, 30,
+	(800, 600), True)
+	while frame_number <= frame_count:
+		frame_number += frame_speedup 
+		vs.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+		print ("Frame number", frame_number)
+		grabbed, image = vs.read()
+		if not grabbed:
+			print("No frame grabbed, exiting..")
+			break
+		img_result, n_idx = detect_from_image(image)
+		writer.write(cv2.resize(img_result,(800, 600)))
+		if n_idx < 4:
+  			frame_speedup = 5 - n_idx
+		else:
+  			frame_speedup = 1
+		fps.update()
+		key = cv2.waitKey(1) & 0xFF
+		if DEBUG_MODE and key == ord("q"):
+			break
+	fps.stop()
+	cv2.destroyAllWindows()
+	writer.release()
+	vs.release()
+	
+if __name__ == '__main__':
+    videostream()
